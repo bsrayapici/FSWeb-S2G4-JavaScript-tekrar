68571b5972b9700975bdf860cd2046a3c8bc9729
busra.yapici <37024872+bsrayapici@users.noreply.github.com>
2025-01-06 12:35:46 +0300

    question 2 solved

--- index.js
+++ index.js
@@ -65,9 +65,11 @@ function CemberinCevresi(r) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(r, pi) {
+  const alan = pi*r*r ; 
+  return alan ;
 }
+console.log(CemberinAlani(15,pi))
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
